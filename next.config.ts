53b3b720d3fedd814d230672357285c5bed441a9
netinho-flavio <antonio@liveseo.com.br>
2025-01-09 11:32:14 -0300

    initial project, configured base routes and environment variables for this project

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,45 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["placehold.jp", "blog-nextjs.seodev.ambienteseo.com.br"],
+    unoptimized: true,
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: `
+              default-src 'self'; 
+              script-src 'self' 'nonce-{generated_nonce}'; 
+              style-src 'self' 'unsafe-inline'; 
+              img-src 'self' data: https:; 
+              connect-src 'self'; 
+              font-src 'self' https: data:; 
+              object-src 'none'; 
+              frame-ancestors 'none'; 
+              base-uri 'self'; 
+              form-action 'self'; 
+            `
+              .replace(/\s{2,}/g, " ")
+              .trim(),
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
