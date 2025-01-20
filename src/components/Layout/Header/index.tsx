d8f74d087c2df336a6e785167d60f75a603e32f8
netinho-flavio <antonio@liveseo.com.br>
2025-01-20 20:43:39 -0300

    fix: ajustado a estrutura de pasta componentes, sombra CSS customizada

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -1,29 +1,33 @@
 'use client'
-import Image from 'next/image'
-import greenLeaf from '../../../../../public/green-leaf.svg'
-import logo from '../../../../../public/logo-real-h.png'
+import Image from 'next/image';
+import greenLeaf from '../../../../public/green-leaf.svg'
+import logo from '../../../../public/logo-real-h.png'
 import { AlignJustify } from 'lucide-react'
 import { useState } from 'react'
+import Link from 'next/link';
 
 function Header() {
 
   const [menuOpen, setMenuOpen] = useState<boolean>();
 
   return (
-    <header className='relative mx-auto container my-4 bg-white px-4 rounded-md flex justify-center items-center shadow-md drop-shadow-md'>
-      <div className='flex justify-between items-center w-full max-w-screen-lg'>
+    <header className='w-4/5 relative mx-auto container my-4 bg-white px-4 rounded-md flex justify-between items-center shadow-custom_shadow  drop-shadow-md'>
+      <div className='flex justify-between items-center w-full'>
         <div>
-          <Image src={logo} alt='Logo da empresa' width={140} height={70} loading='eager'/>
+          <Link href='/' rel='Página inicial' title='Visite a página inicial' tabIndex={0}>
+            <Image src={logo} alt='Logo da empresa' width={140} height={70} loading='eager'/>
+          </Link>
         </div>
         <div className='flex gap-3'>
           {/* Menu desktop */}
-          <ul className='hidden lg:flex gap-4 font-bold text-lg items-center text-black lg:mr-8'>
+          <ul className='hidden xl:flex gap-4 font-bold text-lg items-center text-black lg:mr-8'>
             <li className=''>
               <button className='flex gap-3 bg-fb_green text-white items-center py-2 px-3 text-center rounded-md font-bold'>
                 Sustentabilidade
                 <Image src={greenLeaf.src} alt='Green Leaf' width={13} height={13} loading='eager'/>
               </button>
             </li>
+            <hr className='h-2'/>
             <li>
               <a href='#' className='text-fb_blue_main hover:text-fb_blue'>
                 Institucional
@@ -49,31 +53,31 @@ function Header() {
             </li>
           </ul>
         </div>
-        <div className="absolute top-5 left-0 right-0 w-100">
-          <button className="float-end block lg:hidden" onClick={() => setMenuOpen(!menuOpen)}>
+        <div className="absolute top-1/4 left-0 right-0 w-100 xl:hidden">
+          <button className="float-end block mr-4" onClick={() => setMenuOpen(!menuOpen)}>
             <AlignJustify />
           </button>
         {menuOpen && (
             <ul className='relative container top-5 flex flex-col gap-4 font-bold text-lg items-center text-black bg-white drop-shadow-md shadow-md'>
-              <li>
-                <a href='#' className='text-fb_blue_main hover:text-fb_blue'>
+              <li className='px-5 py-3'>
+                <a href='#' className='text-fb_blue_main hover:text-fb_blue duration-fb_transition_ease'>
             Institucional
                 </a>
               </li>
-              <li>
+              <li className='px-5 py-3'>
                 <a href="#" className='text-fb_blue_main hover:text-fb_blue duration-fb_transition_ease'>Produtos</a>
               </li>
-              <li>
+              <li className='px-5 py-3'>
                 <a href="#" className='text-fb_blue_main hover:text-fb_blue duration-fb_transition_ease'>
             Nossas Marcas
                 </a>
               </li>
-              <li>
+              <li className='px-5 py-3'>
                 <a href="#" className='text-fb_blue_main hover:text-fb_blue duration-fb_transition_ease'>
             Informações
                 </a>
               </li>
-              <li>
+              <li className='px-5 py-3'>
                 <a href="#" className='text-fb_blue_main hover:text-fb_blue duration-fb_transition_ease'>
             Contato
                 </a>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -61,7 +61,10 @@ export default {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
-  		}
+  		},
+			boxShadow: {
+				'custom_shadow': '0 0 4px 0 #00000040'
+			}
   	}
   },
   plugins: [require("tailwindcss-animate")],
