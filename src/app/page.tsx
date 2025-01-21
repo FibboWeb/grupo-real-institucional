1f88bdbdddea3c1e6257386f057b208dfab1e041
Jurazek <jurazek@liveseo.com.br>
2025-01-21 16:15:15 -0300

    feat: create newsletter component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import Newsletter from "@/components/Layout/Newsletter";
 export default function Home() {
   return (
     <div>
       <h1>Página inicial</h1>
+      <Newsletter
+        sectionTitle="Inscreva-se na nossa newsletter"
+        sectionDescription="Receba novidades e informações exclusivas sobre nossos produtos e novidades diretamente no seu e-mail."
+      />
     </div>
   );
 }
