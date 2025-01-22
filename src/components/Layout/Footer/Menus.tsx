0e172cdacfef8b97567529acc4165f87d1b5b85a
Guisposito <guicarlosjoinhas@gmail.com>
2025-01-22 16:27:06 -0300

    css adjusts

--- src/components/Layout/Footer/Menus.tsx
+++ src/components/Layout/Footer/Menus.tsx
@@ -35,7 +35,7 @@ export function Menus({ menusList }: MenusProps) {
             return (
               <div className="menu-section mt-2 lg:mt-0" key={index}>
                 <p
-                  className="flex text-xl justify-between text-fb_blue font-bold uppercase py-2 lg:p-0 w-full text-left"
+                  className="flex text-xl justify-between text-fb_blue font-bold uppercase py-2 lg:p-0 w-full text-left lg:mb-2"
                   onClick={() => setIsOpen(!isOpen)}>
                     {menuName}
                     <span className={`flex items-center cursor-pointer lg:hidden transition-all duration-300 ${ !isOpen ? "rotate-0" : "rotate-180"}`}>
@@ -44,7 +44,7 @@ export function Menus({ menusList }: MenusProps) {
                 </p>
                 <ul className={`${ isOpen ? "max-h-[500px]" : "max-h-0" } lg:max-h-full overflow-hidden transition-all duration-500 ease-in-out`}>
                   {menuItems.map((item) => (
-                    <li key={item.node.id}>
+                    <li className="mb-2" key={item.node.id}>
                       <Link
                         className="text-lg text-white font-bold hover:text-fb_blue duration-300"
                         href={item.node.url}
