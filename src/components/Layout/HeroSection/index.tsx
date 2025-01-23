fdc57ddaeb43fb4fc6c3bc6e34bc0f01cb40809b
Jurazek <jurazek@liveseo.com.br>
2025-01-23 12:37:54 -0300

    feat: create the mobile version for hero section

--- src/components/Layout/HeroSection/index.tsx
+++ src/components/Layout/HeroSection/index.tsx
@@ -13,8 +13,8 @@ export default function HeroSection({ children, ctaLink }: HeroSectionProps) {
     <>
       <div className="flex justify-center bg-hero-image w-full h-full bg-no-repeat bg-cover bg-center">
         <div className="w-full h-full bg-[rgba(3,29,58,0.90)] ">
-          <div className="fb_container mt-32 mb-12">
-            <div className="flex py-12 gap-2">
+          <div className="fb_container mt-12 sm:mt-32 mb-12">
+            <div className="flex flex-col py-12 gap-12 sm:flex-row sm:gap-2">
               <div className="flex flex-1 flex-col justify-center gap-8">
                 {children && (
                   <>
