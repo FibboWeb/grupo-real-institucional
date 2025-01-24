a1a7f2daaa06ce2b0cd300d341d40d7b6a5a769a
SEO dev <142527586+SeoDevLiveseo@users.noreply.github.com>
2025-01-24 17:25:56 -0300

    feat/page-claudio (#16)
    * feat: implementing cards
    
    * feat: implementing info sections and component card section for make spacings standard
    
    * feat: adjust the header to be above other elements
    
    * feat: create hero section
    
    * feat: create the mobile version for hero section
    
    * feat: create a call to action btn component
    
    * feat: implementing slidr section info cards
    
    * feat: hero section and adjusts
    
    * feat: removing infinite
    
    * feat: standardizing texts
    
    * feat: slider breakpoints
    
    ---------
    
    Co-authored-by: Jurazek <jurazek@liveseo.com.br>

---
+++ src/components/Layout/InfoCardsSlider/index.tsx
@@ -0,0 +1,64 @@
+"use client"
+import dynamic from "next/dynamic";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./index.css"
+
+const Slider = dynamic(() => import("react-slick"), { ssr: false });
+
+function InfoCardsSlider({ children }: { children: React.ReactNode }) {
+    const settings = {
+        slidesToShow: 3.2,
+        slidesToScroll: 1,
+        infinite: false,
+        speed: 500,
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 1660,
+                settings: {
+                    slidesToShow: 3.5,
+                    slidesToScroll: 1,
+                    dots: true,
+                },
+            },
+            {
+                breakpoint: 1400,
+                settings: {
+                    slidesToShow: 2.75,
+                    slidesToScroll: 2,
+                },
+            },
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 2.25,
+                    slidesToScroll: 2,
+                },
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                },
+            },
+            {
+                breakpoint: 580,
+                settings: {
+                    slidesToShow: 1.2,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    };
+
+    return (
+        <Slider {...settings}>
+            {children}
+        </Slider>
+    );
+}
+
+export default InfoCardsSlider;
+
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -72,7 +72,8 @@ export default {
       backgroundImage: {
         "footer-image": "url('/images/bg-footer.webp')",
         "newsletter-image": "url('/images/bg-newsletter.webp')",
-		    'fb_gradient': 'linear-gradient(-35deg, #031D3A 0%, #126597 100%)',
+        "fb_gradient": "linear-gradient(-235deg, #031D3A 0%, #126597 100%)",
+        "hero-image": "url('/images/bg-hero-section.webp')",
         'banner-cta': "url('/images/banner-mais-background-blue-_1_.webp')"
       },
       boxShadow: {
