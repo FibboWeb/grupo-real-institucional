5ec4fc120278e432eb94a61309364455f43dc4e1
Jurazek <jurazek@liveseo.com.br>
2025-01-24 14:24:26 -0300

    feat: create board cards and contents for that

---
+++ src/app/quem-somos/BoardCardsContents.tsx
@@ -0,0 +1,29 @@
+import PresidentImg from "@/images/presidente.webp";
+import DrMarioImg from "@/images/dr-mario.webp";
+import DrMarceloImg from "@/images/dr-marcelo.webp";
+import DonaClaudiaImg from "@/images/dona-claudia.webp";
+
+const members = [
+  {
+    img: PresidentImg,
+    name: "Claudio Martins Real",
+    role: "Presidente",
+  },
+  {
+    img: DrMarioImg,
+    name: "Mário Renck Real",
+    role: "Diretor Técnico Industrial",
+  },
+  {
+    img: DrMarceloImg,
+    name: "Marcelo Renck Real",
+    role: "Diretor Comercial",
+  },
+  {
+    img: DonaClaudiaImg,
+    name: "Cláudia Real Leite",
+    role: "Diretora Administrativa",
+  },
+];
+
+export { members };
