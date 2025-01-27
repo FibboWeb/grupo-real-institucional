d6744a194387c4a50192cdeca4fb8de8e1f76b74
Jurazek <jurazek@liveseo.com.br>
2025-01-27 17:16:15 -0300

    feat: create big numbers componente and add that on ambiental page

--- src/app/ambiental/page.tsx
+++ src/app/ambiental/page.tsx
@@ -1,11 +1,14 @@
 import InfiniteCarousel from "@/components/Layout/InfiniteCarousel/InfiniteCarousel";
 import { InfiniteCarouselItems } from "./InfiniteCarouselItems";
+import BigNumbersSection from "@/components/Layout/BigNumbersSection/BigNumbersSection";
+import { BigNumbersItems } from "./BigNumbersItems";
 
 export default function PageAmbiental() {
   return (
     <section className="mt-20">
       <h1>Page ambiental</h1>
       <InfiniteCarousel items={InfiniteCarouselItems} />
+      <BigNumbersSection items={BigNumbersItems} />
     </section>
   );
 }
---
+++ src/components/Layout/BigNumbersSection/BigNumbersSection.tsx
@@ -0,0 +1,27 @@
+import Image, { StaticImageData } from "next/image";
+
+type Item = {
+  icon?: StaticImageData;
+  title?: string;
+  content?: string;
+};
+
+interface BigNumbersSectionProps {
+  items: Item[];
+}
+
+export default function BigNumbersSection({ items }: BigNumbersSectionProps) {
+  return (
+    <section className="bg-fb_green py-12 px-5 sm:px-0">
+      <div className="fb_container w-full flex flex-col sm:flex-row justify-between gap-8 sm:gap-0">
+        {items.map((item, index) => (
+          <div className="flex flex-col items-center sm:w-[25%]" key={index}>
+            {item.icon && <Image src={item.icon} alt="" className="bg-white p-4 rounded-full" width={80} />}
+            {item.title && <h3 className="text-white text-4xl font-bold">{item.title}</h3>}
+            {item.content && <p className="text-white font-medium text-lg text-center uppercase">{item.content}</p>}
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
