f759755b7164007ac96f72835fc3694c3ee65175
Jurazek <jurazek@liveseo.com.br>
2025-01-27 16:08:31 -0300

    feat: adjust the infinite carousel items

--- src/app/ambiental/InfiniteCarouselItems.tsx
+++ src/app/ambiental/InfiniteCarouselItems.tsx
@@ -1,28 +1,28 @@
 import LeafIcon from "@/public/icons/LeafIcon.svg";
 const InfiniteCarouselItems = [
   {
-    icon: LeafIcon,
     content: "Lorem ipsum dolor sit amet",
   },
   {
-    icon: LeafIcon,
+    content: LeafIcon,
+  },
+  {
     content: "Lorem ipsum dolor sit amet",
   },
   {
-    icon: LeafIcon,
+    content: LeafIcon,
+  },
+  {
     content: "Lorem ipsum dolor sit amet",
   },
   {
-    icon: LeafIcon,
+    content: LeafIcon,
+  },
+  {
     content: "Lorem ipsum dolor sit amet",
   },
   {
-    icon: LeafIcon,
-    content: "Lorem ipsum dolor sit amet",
-  },
-  {
-    icon: LeafIcon,
-    content: "Lorem ipsum dolor sit amet",
+    content: LeafIcon,
   },
 ];
 
