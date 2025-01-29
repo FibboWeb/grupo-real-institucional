6b5b01a4169dd48cbd0695312f738764b3b6bfea
SEO dev <142527586+SeoDevLiveseo@users.noreply.github.com>
2025-01-29 12:11:15 -0300

    feat: Bringing a list of products from the realH Line (#72)

--- src/app/linhas/(componentes)/index.tsx
+++ src/app/linhas/(componentes)/index.tsx
@@ -1,21 +1,19 @@
-"use server";
-import { getProducts } from "@/api/produtos/produtos";
 import CardProduct from "@/components/CardProdutos";
-import { mockProducts } from "@/constants/linhas";
-
+import { getProducts } from "@/lib/getProducts";
 export default async function GridProduct() {
-  const products = await getProducts();
-
-  return (
-    <>
-      <div className="grid grid-cols-1 md:grid-cols-3 xl:grid-cols-4 gap-6 items-center justify-center w-full">
-        {mockProducts.map((product) => (
-          <div key={product.id} className="mb-8">
-            <CardProduct produto={product} />
-          </div>
-        ))}
-      </div>
-      <div>{/* <Pagination  /> */}</div>
-    </>
-  );
+  const products = await getProducts(["linha-nutricao", "linha-saude"]);
+    return (
+      <>
+        <div className="w-full">
+          {products.data?.edges.map((edge, index) => (
+            <div key={index} className="mb-8 min-h-[520px]">
+              {/* Passando os detalhes do produto para o componente CardProduct */}
+              <CardProduct product={[edge.node.camposLinhas.gradeProdutos]} />
+            </div>
+          ))}
+        </div>
+        <div>{/* <Pagination /> */}</div>
+      </>
+    );
 }
+        
--- src/lib/getProducts.ts
+++ src/lib/getProducts.ts
@@ -1,18 +1,27 @@
 "use server"
-export async function getProducts() {
+
+import { GET_PRODUCT_BY_LINES } from "@/graphql/linhas";
+import { CardProductPropsAPI } from "@/types/produto";
+import { client } from "./apollo-client";
+
+export async function getProducts([linha01, linha02]): Promise<CardProductPropsAPI> {
   try {
-    const res = await fetch(`${ process.env.WP_URL }/produto`, {
-      cache: 'no-store', // Use 'force-cache' ou 'no-store' dependendo do comportamento desejado
-    });
-    const products = await res.json();
-    return products
+    const ProductsBylines = await client.query({
+          query: GET_PRODUCT_BY_LINES,
+          variables: { nameIn: [linha01, linha02] },
+        });
+        return {
+          data: ProductsBylines.data.linhas,
+          loading: false, // or some other value that makes sense for your app
+          networkStatus: 7,
+        }; 
   } catch (error) {
-    console.error('Erro ao buscar produtos:', error);
     return error
   }
+
 }
 
-export async function getProductPerSlug(slug: string): Promise<WordPressPost> {
+export async function getProductPerSlug(slug: string): Promise<any> {
   try {
     const res = await fetch(`${ process.env.WP_URL }/produto?slug=${ slug }`, {
       cache: 'no-store', // Use 'force-cache' ou 'no-store' dependendo do comportamento desejado
