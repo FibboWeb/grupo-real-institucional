bc7577e49aae12d584cf5cb6770080fa33308485
SEO dev <seodev@liveseo.com.br>
2025-01-29 12:04:08 -0300

    feat: implementing section last eventos

--- src/app/historia/page.tsx
+++ src/app/historia/page.tsx
@@ -1,39 +1,43 @@
 import Breadcrumb from "@/components/BreadCrumb";
-import LastPostsNoticias from "@/components/Layout/LastPostsNoticias"
-import { getLastPostsNoticias } from "@/lib/getLastPostsNoticias";
+import LastPostsEventos from "@/components/Layout/LastPostsEventos"
+import { getLastPostsEventos } from "@/lib/getLastPostsEventos";
 
 export default async function Historia() {
-    const queriedLastPostsNoticias = await getLastPostsNoticias();
-    const fetchedLastPostsNoticias = queriedLastPostsNoticias.props.nodes;
+    const queriedLastPostsEventos = await getLastPostsEventos();
+    const fetchedLastPostsEventos = queriedLastPostsEventos.props.nodes;
     function CardSection({ children }: { children: React.ReactNode }) {
         return (
-            <div className="text-fb_text_gray relative">
-                <div className="flex flex-wrap lg:flex-nowrap gap-5 lg:gap-10">{children}</div>
-            </div>
+            <section className="max-w-full">
+                <div className="fb_container overflow-hidden">
+                    {children}
+                </div>
+            </section>
         )
     }
 
     return (
         <div className='relative mt-36'>
-            <section className="max-w-full">
-                <div className="fb_container overflow-hidden">
+            <CardSection>
                     <Breadcrumb
                         activeClasses="text-fb_gray_bread"
                         containerClasses="flex py-5"
                         listClasses="mx-2 font-bold text-fb_gray_bread hover:text-fb_blue duration-300 "
                         capitalizeLinks
                     />
+            </CardSection>
+            <CardSection>
+                <div className={`hero-category bg-hero-page-historia bg-no-repeat bg-cover bg-center h-56 xl:h-60 rounded-2xl`}></div>
+            </CardSection>
+            <CardSection>
+                <div className="flex flex-col py-4 lg:py-8">
+                    <h1 className="text-fb_blue_main text-xl lg:text-3xl font-bold text-center pb-2">Confira em breve todos os acontecimentos do evento</h1>
+                    <span className="text-[#9A9A9A] font-semibold text-center text-lg w-full">Fotos, vídeos, notícias e muito mais...</span>
                 </div>
-            </section>
-            <section className="max-w-full">
-                <div className="fb_container overflow-hidden">
-                    <div className="flex flex-col py-4">
-                        <h1 className="text-fb_blue_main text-xl lg:text-3xl font-bold text-center">Confira em breve todos os acontecimentos do evento</h1>
-                        <span className="text-[#9A9A9A] font-semibold text-center text-lg w-full">Fotos, vídeos, notícias e muito mais...</span>
-                    </div>
-                    <LastPostsNoticias fetchedLastPosts={fetchedLastPostsNoticias} />
+                <div className="py-8">
+                    <LastPostsEventos fetchedLastPosts={fetchedLastPostsEventos} />
                 </div>
-            </section>
+
+            </CardSection>
         </div>
     )
 }
