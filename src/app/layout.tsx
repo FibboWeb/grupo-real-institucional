d6ef9dff8ce58677ad94d4d02ccda1b85c3099cf
SEO dev <142527586+SeoDevLiveseo@users.noreply.github.com>
2025-01-30 15:22:13 -0300

    Feat/integrate google (#93)
    * fix: adjust in code many locals
    
    * fix; adjust in page historia
    
    * fix: adjust in page home
    
    * chore: merge da branch development
    
    * fix: adjust in constants.ts
    
    * fix: adjust in constant home
    
    * feat: add SERP in home
    
    * fix: adjust in home page noticias
    
    * fix: adjust in multiple points.
    
    * feat: created account google search console and code  added for tracking
    
    * feat: added tag GTM in code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import Footer from "../components/Layout/Footer";
 import "../styles/globals.css";
 import Header from "../components/Layout/Header";
 import WhatsappButton from "@/components/Layout/WhatsappButton";
+import { GoogleTagHead, GoogleTagBody } from "@/components/GoogleTag";
 
 const poppins = Poppins({
   weight: ["300", "400", "500", "600", "700"],
@@ -13,6 +14,16 @@ const poppins = Poppins({
 export const metadata: Metadata = {
   title: "Página inicial - Grupo real H",
   description: "40 anos construindo gerações reais.",
+  verification: {
+    google: "kYyHL7QQ_V8N1msmt0rXzo3aBZEmIYZ1xLjm28gHFqQ",
+  },
+  openGraph: {
+    title: "Página inicial - Grupo real H",
+    description: "40 anos construindo gerações reais.",
+    images: ["/favicon.ico"],
+    locale: "pt_BR",
+    siteName: "Grupo real H",
+  }
 };
 
 export default function RootLayout({
@@ -21,8 +32,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" data-lt-installed="true" suppressHydrationWarning={true}>
+    <html lang="pt-BR" data-lt-installed="true" suppressHydrationWarning={true}>
+      <head>
+        <GoogleTagHead />
+      </head>
       <body className={`${poppins.className} antialiased`}>
+        <GoogleTagBody />
         <Header />
         {children}
         <Footer />
