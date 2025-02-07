7c6f91d32c3bb19e1bbd0310ef5954f2db893e26
fibboweb <seodev@liveseo.com.br>
2025-02-07 17:55:55 -0300

    fix: add props in btn call to action

--- src/components/Layout/Buttons/BtnCallToAction/BtnCallToAction.tsx
+++ src/components/Layout/Buttons/BtnCallToAction/BtnCallToAction.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import "./index.css";
 import WhiteLeafIcon from "@/public/icons/white-leaf.svg";
 import ArrowIcon from "@/public/icons/arrow-right.svg";
+import { cn } from "@/lib/utils";
 
 interface BtnCallToActionProps {
   ctaLink?: string;
@@ -10,9 +11,11 @@ interface BtnCallToActionProps {
   icon?: StaticImageData;
   color?: "fb_blue_button" | "fb_green_button" | "white";
   onClick?: () => void;
+  classCssForBTN?: string;
+  showIcon?: boolean;
 }
 
-export default function BtnCallToAction({ ctaLink, content, icon, color = "fb_blue_button", onClick }: BtnCallToActionProps) {
+export default function BtnCallToAction({ ctaLink, content, icon, color = "fb_blue_button", onClick, classCssForBTN, showIcon = true  }: BtnCallToActionProps) {
   let text;
   let textHover;
   let bg;
@@ -52,7 +55,7 @@ export default function BtnCallToAction({ ctaLink, content, icon, color = "fb_bl
       {ctaLink ? (
         <Link
           href={ctaLink}
-          className={`${text} ${textHover} ${bg} ${bgHover} ${border} group btn-container inline-flex items-center gap-4 py-3 px-4 rounded text-base font-semibold uppercase border border-solid transition-all duration-300`}
+          className={cn([`${text} ${textHover} ${bg} ${bgHover} ${border} group btn-container inline-flex items-center gap-4 py-3 px-4 rounded text-base font-semibold uppercase border border-solid transition-all duration-300`, `${classCssForBTN}`])}
         >
           <span>{content}</span>
           <div className="shrink-0">
@@ -71,15 +74,17 @@ export default function BtnCallToAction({ ctaLink, content, icon, color = "fb_bl
           className={`${text} ${textHover} ${bg} ${bgHover} ${border} group btn-container inline-flex items-center gap-4 py-3 px-4 rounded text-base font-semibold uppercase border border-solid transition-all duration-300`}
         >
           <span>{content}</span>
+          { showIcon && (
           <div className="shrink-0">
             <Image
               src={iconBtn}
               alt="Arrow Icon"
               width={24}
               height={24}
-              className={`btn-logo ${color == "white" ? "bg-[#cccccc] group-hover:bg-white" : "filter-image"} ${color == "fb_blue_button" ? "bg-white group-hover:bg-[#cccccc]" : "filter-image"} rounded-full p-1 transition-all duration-300`}
+              className={`btn-logo ${color == "white" ? "bg-[#cccccc] group-hover:bg-white" : "filter-image"} ${color == "fb_blue_button" ? "bg-white group-hover:bg-[#cccccc]" : "filter-image"}  rounded-full p-1 transition-all duration-300`}
             />
-          </div>
+            </div>  
+          )}
         </button>
       )}
     </>
