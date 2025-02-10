3ed4eb5e7caa2d294d4ac91550a0b0efb87f8ac1
fibboweb <seodev@liveseo.com.br>
2025-02-10 17:21:51 -0300

    fix: adjust in text of hero

--- src/app/quem-somos/childrenHeroSection.tsx
+++ src/app/quem-somos/childrenHeroSection.tsx
@@ -9,3 +9,15 @@ export default function childrenHeroSection(): React.ReactNode {
     </div>
   );
 }
+
+export function childrenHeroSectionContent() {
+  return (
+    <div className="flex flex-col gap-6 my-4 w-full md:w-2/5 min-h-[72px]">
+      <h1 className="text-[42px] text-white font-bold">Grupo Real</h1>
+      <p className="text-white">
+        O <strong>Grupo REAL</strong> reúne marcas que transformam desafios em oportunidades. Descubra nosso compromisso
+        com a <strong>qualidade e sustentabilidade.</strong>
+      </p>
+    </div>
+  );
+}
