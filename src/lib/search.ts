625fbcfbf0713b6c439d6cce81d9d8117532f9d0
fibboweb <seodev@liveseo.com.br>
2025-02-11 10:38:35 -0300

    fix: add console.log in search function

--- src/lib/search.ts
+++ src/lib/search.ts
@@ -1,14 +1,23 @@
+import { error } from "console";
+
 export async function search(search: string, page = 1) {
   const encodedString = encodeURI(search);
-  const response = await fetch(`${process.env.WP_URL_API}posts?search=${encodedString}&per_page=10&page=${page}&_embed=wp:featuredmedia,wp:term=1,author`);
-  const categoriesName = []
-  const totalPages = response.headers.get("X-WP-TotalPages");
-  const totalPosts = response.headers.get("X-WP-Total");
-  const data = await response.json();
- data.map( async (value, index, array) => {
-    const res = await fetch(`${process.env.WP_URL_API}categories?include=${value.categories[0]}&_embed=author`)
-    const categoriesFetched = await res.json()
-    categoriesName.push(categoriesFetched[0].name)
-  })
-  return { data, totalPages, totalPosts, categoriesName };
+  console.log(encodedString);
+  try {
+    const response = await fetch(
+      `${process.env.WP_URL_API}posts?search=${encodedString}&per_page=10&page=${page}&_embed=wp:featuredmedia,wp:term=1,author`,
+    );
+    const categoriesName = [];
+    const totalPages = response.headers.get("X-WP-TotalPages");
+    const totalPosts = response.headers.get("X-WP-Total");
+    const data = await response.json();
+    data.map(async (value, index, array) => {
+      const res = await fetch(`${process.env.WP_URL_API}categories?include=${value.categories[0]}&_embed=author`);
+      const categoriesFetched = await res.json();
+      categoriesName.push(categoriesFetched[0].name);
+    });
+    return { data, totalPages, totalPosts, categoriesName };
+  } catch (error) {
+    console.log(error)
+  }
 }
