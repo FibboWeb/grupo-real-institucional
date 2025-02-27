6df15fc2597b78b126d6764d333535c57d5591c8
fibboweb <seodev@liveseo.com.br>
2025-02-27 15:49:27 -0300

    fix: adjust font weigth

--- src/app/representantes/page.tsx
+++ src/app/representantes/page.tsx
@@ -37,15 +37,11 @@ export default function RepresentantesPage() {
   }, []);
 
   async function handleFilter(category) {
-    setSelectedCategory(category);
-    const representantesList = await getRepresentantes();
-    const allRepresentantes = representantesList;
     if (category) {
-      const filteredRepresentantes = allRepresentantes.props.filter((item) => item.categoriaName.includes(category));
-      setRepresentantes(filteredRepresentantes);
-    } else {
-      setRepresentantes(allRepresentantes.props);
+      setOpenCategory(category);
+      setSelectedCategory(category);
     }
+    return;
   }  
   const handleOpen = (category) => {
     setOpenCategory(openCategory === category ? null : category);
@@ -78,8 +74,7 @@ export default function RepresentantesPage() {
               <li>
                 <Button
                   variant="ghost"
-                  disabled
-                  className={`w-full justify-start text-gray-600 hover:text-gray-900 ${selectedCategory === "" ? "font-bold" : ""}`}
+                  className={`w-full justify-start text-gray-600 hover:text-gray-900 ${selectedCategory !== "" ? "font-bold" : ""}`}
                   onClick={() => handleFilter("")}
                 >
                   Todas
@@ -88,7 +83,6 @@ export default function RepresentantesPage() {
               {categories.map((category, index) => (
                 <li key={index + Math.random()}>
                   <Button
-                    disabled
                     variant="ghost"
                     className={`w-fit justify-start text-gray-600 hover:text-gray-900 ${selectedCategory === category ? "font-bold" : ""}`}
                     onClick={() => handleFilter(category)}
@@ -103,7 +97,7 @@ export default function RepresentantesPage() {
         <section className="flex flex-col gap-4 w-full">          
           {categories.map((category, index) => (
             <div key={category + index}>
-              <div className="cursor-pointer w-full justify-between items-center flex border border-fb_blue_main hover:border-fb_blue_main/80 rounded-lg p-4 hover:relative hover:scale-[101%] hover:-top-2 hover:drop-shadow-lg transition duration-400 ease-in-out" onClick={() => handleOpen(category)}>
+              <div className="cursor-pointer w-full justify-between items-center flex border border-fb_blue_main hover:border-fb_blue_main/80 rounded-lg p-4 hover:relative hover:scale-[101%] hover:drop-shadow-lg transition duration-400 delay-100 ease-in-out" onClick={() => handleOpen(category)}>
                 <h2 
                   key={index + category}
                   className="text-xl font-semibold"
@@ -114,17 +108,17 @@ export default function RepresentantesPage() {
                   <ChevronDownIcon className={`w-6 h-6 ${ openCategory === category ? 'rotate-180' : ''} transition-transform duration-300 ease-in-out`} />
                 </span>
               </div>
-              <div className="w-full border " data-state={ openCategory === category ? 'open' : 'closed'}>
-                <div className="flex flex-wrap border-b border-gray-300 animate-accordion-down">
+              <div className={`w-full border ${openCategory === category ? 'block transition-all duration-300 ease-in-out' : 'max-h-0 invisible h-0 w-0 overflow-hidden'}`} data-state={openCategory === category ? 'open' : 'closed'}>
+                <div className={`flex flex-wrap border-b border-gray-300 ${openCategory === category ? 'animate-fade-in' : ''}`}>
                   {representantes
-                  // se a categoria for linha saude renderizar dentro de saúde animal
+                    // se a categoria for linha saude renderizar dentro de saúde animal
                     .filter((representante) => (representante.categoriaId.includes(category) || (representante.categoriaId.includes("Linha Saúde") && category === "Saúde Animal"))) 
                     .map((representante) => (
                       <div key={representante.title} className="w-full lg:w-1/2 2xl:w-1/3 p-4 border-b border-gray-300">
                         <div className="flex flex-col gap-1">
                           <h3 className="text-2xl font-semibold mb-2">{representante.title.replace("&#038;", "&").replace("&#8211;", ".")}</h3>
                           <div className="inline-block">
-                            { representante.categoriaId && representante.categoriaId.filter(item => item !== "Linha Saúde").map((item, index) => (
+                            {representante.categoriaId && representante.categoriaId.filter(item => item !== "Linha Saúde").map((item, index) => (
                               <span key={item} className={`text-base ${categoriasColors[item]}`}>
                                 {item}{index < representante.categoriaId.length - 1 && item !== "Saúde Animal" ? " - " : ""}
                               </span>
