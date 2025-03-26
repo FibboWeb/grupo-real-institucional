6633653857c969faf4bde99a55a981dc00803669
fibboweb <seodev@liveseo.com.br>
2025-03-26 08:32:06 -0300

    fix; adjust spacing between elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -85,7 +85,7 @@ export default async function Home() {
         <h2 className="text-center text-fb_blue_main text-4xl font-bold">Depoimentos</h2>
         <SliderTestimonials testimonial={testimoniaslInfo} />
       </section>
-      <section className="fb_container flex flex-col md:flex-row justify-between items-center gap-5 md:gap-0 my-2">
+      <section className="fb_container flex flex-col md:flex-row justify-around items-center gap-5 md:gap-0 my-2">
         <div className="flex flex-col justify-start items-center my-2 gap-4">
           <h3 className="text-xl md:text-3xl font-bold text-center">Acesse nosso CMR de vendas</h3>
           <BtnCallToAction
