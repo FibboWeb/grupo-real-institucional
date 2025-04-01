d37eadc3999342e065fbed3896290f7744d530bc
fibboweb <seodev@liveseo.com.br>
2025-04-01 17:17:58 -0300

    fix: removing duplicate carrousel in page home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -56,7 +56,6 @@ export default async function Home() {
       <section className="max-w-full">
         <div className="fb_container overflow-hidden">
           <LastPostsNoticias fetchedLastPosts={postsMesclados ?? []} />
-          <LastPostsNoticias fetchedLastPosts={postsMesclados ?? []} />
         </div>
       </section>
       <section className="">
