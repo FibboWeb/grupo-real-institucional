03bcd22bc5d48d89e7d1facbc7305af4beb331f6
fibboweb <seodev@liveseo.com.br>
2025-04-25 08:03:16 -0300

    fix: alter "Grupo Real H" for "Grupo Real"

--- src/app/quem-somos/BoardCardsContents.tsx
+++ src/app/quem-somos/BoardCardsContents.tsx
@@ -14,14 +14,14 @@ const members = [
   {
     img: DrMarioImg,
     name: "Mário Renck Real",
-    role: "Médico Veterinário, Fundador e Sócio-Proprietário do Grupo Real H",
+    role: "Médico Veterinário, Fundador e Sócio-Proprietário do Grupo Real",
     description: "<p>Nascido em Porto Alegre (RS) em 1956, Mário Renck Real é Médico Veterinário formado pela Universidade Federal do Rio Grande do Sul (UFRGS) em 1979. Visionário e inovador, é um dos fundadores e sócio-proprietários do <strong>Grupo Real</strong>, onde desempenha um papel fundamental no desenvolvimento de soluções tecnológicas para a saúde e nutrição animal.</p> <p>Buscando aprofundar seus conhecimentos, especializou-se em homeopatia veterinária no <strong>Instituto Homeopático François Lamasson</strong>, em Ribeirão Preto (SP), nos anos de 1986 e 1987. Como sócio-proprietário e diretor do Grupo Real, continua impulsionando o crescimento da empresa e o desenvolvimento de soluções eficientes para a pecuária, sempre alinhado à sustentabilidade, ao bem-estar animal e à eficiência produtiva.</p>"
   },
   {
     img: DrMarceloImg,
     name: "Marcelo Renck Real",
     role: "Diretor Comercial",
-    description: "<p>Nasceu em Porto Alegre (RS), 24 de abril de 1961, Marcelo Renck Real é Médico Veterinário formado pela <strong>Universidade Federal de Mato Grosso do Sul (UFMS)</strong> em 1983. Com uma trajetória consolidada na medicina veterinária e no setor agropecuário, é um dos <strong>fundadores e sócio-proprietários do Grupo Real H</strong>, onde atua como Diretor Comercial.</p><p>Especializou-se em <strong>Homeopatia Veterinária</strong> pelo <strong>Instituto François Lamasson</strong>, em Ribeirão Preto (SP), entre 1997 e 1998. Posteriormente, ampliou seus conhecimentos em gestão e estratégia empresarial ao cursar um <strong>MBA em Gestão Empresarial</strong> pela <strong>Fundação Getúlio Vargas (FGV)</strong> entre 1999 e 2000.</p><p>Com uma carreira pautada pela inovação e pelo compromisso com o desenvolvimento do agronegócio, Marcelo Renck Real segue à frente do <strong>Grupo Real H</strong>, impulsionando novas tecnologias para a nutrição e saúde animal, sempre com foco na eficiência produtiva e na sustentabilidade.</p>"
+    description: "<p>Nasceu em Porto Alegre (RS), 24 de abril de 1961, Marcelo Renck Real é Médico Veterinário formado pela <strong>Universidade Federal de Mato Grosso do Sul (UFMS)</strong> em 1983. Com uma trajetória consolidada na medicina veterinária e no setor agropecuário, é um dos <strong>fundadores e sócio-proprietários do Grupo Real</strong>, onde atua como Diretor Comercial.</p><p>Especializou-se em <strong>Homeopatia Veterinária</strong> pelo <strong>Instituto François Lamasson</strong>, em Ribeirão Preto (SP), entre 1997 e 1998. Posteriormente, ampliou seus conhecimentos em gestão e estratégia empresarial ao cursar um <strong>MBA em Gestão Empresarial</strong> pela <strong>Fundação Getúlio Vargas (FGV)</strong> entre 1999 e 2000.</p><p>Com uma carreira pautada pela inovação e pelo compromisso com o desenvolvimento do agronegócio, Marcelo Renck Real segue à frente do <strong>Grupo Real</strong>, impulsionando novas tecnologias para a nutrição e saúde animal, sempre com foco na eficiência produtiva e na sustentabilidade.</p>"
   },
   {
     img: DonaClaudiaImg,
