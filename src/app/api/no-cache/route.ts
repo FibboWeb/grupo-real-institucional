0cbace081015fd4b5fad03aa9501affab50c40cb
fibboweb <seodev@liveseo.com.br>
2025-05-15 09:30:30 -0300

    fix: resolve error in build

--- src/app/api/no-cache/route.ts
+++ src/app/api/no-cache/route.ts
@@ -1,4 +1,7 @@
-export default function GET(req, res) {
-  res.setHeader('Cache-Control', 'no-store, max-age=0');
-  res.json({ message: 'This response is not cached by Vercel.' });
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(req: NextRequest) {
+  const response = NextResponse.json({ message: 'This response is not cached by Vercel.' });
+  response.headers.set('Cache-Control', 'no-store, max-age=0');
+  return response;
 }
