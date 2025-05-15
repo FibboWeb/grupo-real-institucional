e71aed39c8d783dedafc25b4271f7e392d90ed84
fibboweb <seodev@liveseo.com.br>
2025-05-15 09:26:46 -0300

    fix: rename endpoint api

--- src/app/api/no-cache/route.ts
+++ src/app/api/no-cache/route.ts
@@ -1,4 +1,4 @@
-export default function handler(req, res) {
+export default function GET(req, res) {
   res.setHeader('Cache-Control', 'no-store, max-age=0');
   res.json({ message: 'This response is not cached by Vercel.' });
 }
